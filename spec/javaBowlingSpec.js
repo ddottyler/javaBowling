10acb5edc03921631b70e484dadf3e95e786443e
Dan T <dp.tyler@hotmail.co.uk>
2021-02-12 11:40:18 +0000

    Scorecard working with strikes and frames

--- spec/javaBowlingSpec.js
+++ spec/javaBowlingSpec.js
@@ -6,12 +6,26 @@ describe('BowlingGame', function() {
     game = new BowlingGame();
   });
 
-  it('should give frame 1 a score of 7', function() {
-    game.roll(4)
-    game.roll(3)
-    expect(game._score).toEqual(7);
+  it('should create a frame hash when two balls rolled', function() {
+    game.roll(4, 3)
+    expect(game._frames.length).toEqual(1);
   });
 
-  it('should give frame 2 a score of ')
+  it('should give frame 1 a score of 7', function() {
+    game.roll(4, 3)
+    expect(game.framescore(1-1)).toEqual(7);
+  });
+  
+  it('should give the a total score of 7 after one frame', function() {
+    game.roll(4, 3)
+    expect(game.totalscore()).toEqual(7);
+  });
+
+  it('should return a total score of 100 after 10 frames', function() {
+    for (let i = 0; i < 10; i++) {
+      game.roll(10,0)
+    }
+    expect(game.totalscore()).toEqual(100);
+  });
 
 });
--- src/javaBowling.js
+++ src/javaBowling.js
@@ -4,10 +4,21 @@ class BowlingGame {
 
   constructor() {
     this._score = 0
+    this._frames = []
   }
 
-  roll(pins) {
-    this._score += pins
+  roll(roll1, roll2) {
+    this._frames.push([roll1, roll2]);
+  }
+
+  framescore(frame) {
+    let framescore = this._frames[frame][0] + this._frames[frame][1]
+    return framescore
+  }
+
+  totalscore() {
+    this._frames.forEach(frame => this._score += frame[0] += frame[1])
+    return this._score
   }
 
 }
