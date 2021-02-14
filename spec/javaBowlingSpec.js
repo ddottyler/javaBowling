7a39d5ba91a713b91926c98dcf79b8b6a533f4df
Dan T <dp.tyler@hotmail.co.uk>
2021-02-14 13:29:03 +0000

    fresh approach taken

--- spec/javaBowlingSpec.js
+++ spec/javaBowlingSpec.js
@@ -6,26 +6,23 @@ describe('BowlingGame', function() {
     game = new BowlingGame();
   });
 
-  it('should create a frame hash when two balls rolled', function() {
-    game.roll(4, 3)
-    expect(game._frames.length).toEqual(1);
-  });
+  it('should roll the ball', function() {
+    game.roll(7)
+    expect(game.score()).toEqual(7)
+  })
 
-  it('should give frame 1 a score of 7', function() {
-    game.roll(4, 3)
-    expect(game.framescore(1-1)).toEqual(7);
-  });
-  
-  it('should give the a total score of 7 after one frame', function() {
-    game.roll(4, 3)
-    expect(game.totalscore()).toEqual(7);
-  });
-
-  it('should return a total score of 100 after 10 frames', function() {
-    for (let i = 0; i < 10; i++) {
-      game.roll(10,0)
+  it('should return calculate a gutter game', function() {
+    for (let i = 0; i < 20; i++) {
+      game.roll(0)
     }
-    expect(game.totalscore()).toEqual(100);
+    expect(game.score()).toEqual(0);
   });
 
-});
+  it('can score a game of ones', function() {
+    for (let i = 0; i < 20; i++) {
+      game.roll(1)
+    }
+    expect(game.score()).toEqual(20);
+  });
+
+})
--- src/javaBowling.js
+++ src/javaBowling.js
@@ -4,21 +4,15 @@ class BowlingGame {
 
   constructor() {
     this._score = 0
-    this._frames = []
   }
 
-  roll(roll1, roll2) {
-    this._frames.push([roll1, roll2]);
-  }
+roll(pinsdown) {
+  this._score += pinsdown
+}
 
-  framescore(frame) {
-    let framescore = this._frames[frame][0] + this._frames[frame][1]
-    return framescore
-  }
-
-  totalscore() {
-    this._frames.forEach(frame => this._score += frame[0] += frame[1])
-    return this._score
-  }
+score() {
+  return this._score;
+}
 
 }
+
